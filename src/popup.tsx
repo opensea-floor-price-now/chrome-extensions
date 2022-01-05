e8331d9be6a221b5f485aae93a0cd9a4f9d02a4c
牛叠谊 <18715156450@163.com>
2022-01-05 15:40:23 +0800

    feat: init

--- src/popup.tsx
+++ src/popup.tsx
@@ -2,50 +2,28 @@ import React, { useEffect, useState } from "react";
 import ReactDOM from "react-dom";
 
 const Popup = () => {
-  const [count, setCount] = useState(0);
-  const [currentURL, setCurrentURL] = useState<string>();
+  const [firstNftPrice, setFirstNftPrice] = useState(0);
+  const [loading, setLoading] = useState(true);
 
   useEffect(() => {
-    chrome.action.setBadgeText({ text: count.toString() });
-  }, [count]);
+    chrome.action.setBadgeText({ text: firstNftPrice.toString() });
+  }, [firstNftPrice]);
 
-  useEffect(() => {
-    chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
-      setCurrentURL(tabs[0].url);
-    });
-  }, []);
-
-  const changeBackground = () => {
-    chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
-      const tab = tabs[0];
-      if (tab.id) {
-        chrome.tabs.sendMessage(
-          tab.id,
-          {
-            color: "#555555",
-          },
-          (msg) => {
-            console.log("result message:", msg);
-          }
-        );
-      }
-    });
-  };
+  setTimeout(() => {
+    setLoading(false);
+  }, 1000);
 
   return (
-    <>
-      <ul style={{ minWidth: "700px" }}>
-        <li>Current URL: {currentURL}</li>
-        <li>Current Time: {new Date().toLocaleTimeString()}</li>
-      </ul>
+    <div style={{ width: "300px", maxHeight: "540px", overflow: "auto", padding: "10px 5px" }}>
+      {loading && <div>Loading</div>}
+      {!loading && <div>
       <button
-        onClick={() => setCount(count + 1)}
         style={{ marginRight: "5px" }}
       >
-        count up
-      </button>
-      <button onClick={changeBackground}>change background</button>
-    </>
+        Refresh
+      </button></div>}
+
+    </div>
   );
 };
 
