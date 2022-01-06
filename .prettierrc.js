2f089463cf861b670d300aaf416d287a8150258d
牛叠谊 <18715156450@163.com>
2022-01-06 08:53:09 +0800

    feat: add prettier config

---
+++ .prettierrc.js
@@ -0,0 +1,19 @@
+module.exports = {
+    printWidth: 80,
+    tabWidth: 2,
+    useTabs: false,
+    singleQuote: true,
+    trailingComma: 'none',
+    bracketSpacing: true,
+    proseWrap: 'never',
+    endOfLine: 'lf',
+    jsxSingleQuote: false,
+    overrides: [
+      {
+        files: '.prettierrc',
+        options: {
+          parser: 'json'
+        }
+      }
+    ]
+};
